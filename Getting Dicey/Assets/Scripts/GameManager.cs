2d23e6fdc4bbbfe388a56b7fa7087ad9407b5a33
JoshClark7 <joshtclark7@gmail.com>
2023-02-03 11:53:07 -0500

    Added more functionality to the scene

--- Getting Dicey/Assets/Scripts/GameManager.cs
+++ Getting Dicey/Assets/Scripts/GameManager.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text moneyLabel;
 
+    [SerializeField]
+    private InputAction rollDiceAction;
+
     private float money;
-    private int index;
 
     public float Money
     {
@@ -24,6 +27,12 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void Start()
     {
+        rollDiceAction.Enable();
+        rollDiceAction.performed += (InputAction.CallbackContext obj) =>
+        {
+            RollButton();
+        };
+
         DicePreset dicePreset = Resources.Load<DicePreset>("DicePresets/D6Preset");
         dice.Add(dicePreset.GetDiceObject());
         dice.Add(dicePreset.GetDiceObject());
@@ -32,6 +41,7 @@ public class GameManager : MonoBehaviour
             i.gameObject.SetActive(false);
         }
     }
+
 
     /// <summary>
     /// Called every frame
@@ -44,15 +54,19 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Called when the roll button is pressed.
     /// </summary>
-    public void Roll()
+    public void RollButton()
     {
-        dice[index].gameObject.SetActive(true);
-        dice[index].gameObject.transform.position = new Vector3(0f, 0f, 0f);
-        dice[index].gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f));
-        index++;
-        if (index >= dice.Count)
+        foreach (Die i in dice)
         {
-            index = 0;
+            RollDie(i);
         }
+    }
+
+    private void RollDie(Die die)
+    {
+        die.gameObject.SetActive(true);
+        die.gameObject.transform.position = new Vector3(0f, 35f, -55f);
+        die.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+        die.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 35f);
     }
 }
